a77775175aed966fba94a1b51c732f7e46e54f6e
guilherme <jguicf@gmail.com>
2021-09-22 09:52:23 -0400

    feat: add project

---
+++ animationsInSwift/animationsInSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  animationsInSwift
+//
+//  Created by João Guilherme on 22/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ animationsInSwift/animationsInSwift/animationsInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsInSwiftApp.swift
+//  animationsInSwift
+//
+//  Created by João Guilherme on 22/09/21.
+//
+
+import SwiftUI
+
+@main
+struct animationsInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
